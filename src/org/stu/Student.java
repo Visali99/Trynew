004685c47fcc6170adde88b96e66e2f49f7d1a21
Visali99 <visalivenkatesan99@gamil.com>
2022-10-30 14:23:11 +0530

    adding some student details

---
+++ src/org/stu/Student.java
@@ -0,0 +1,102 @@
+package org.stu;
+
+public class Student {
+     
+	
+      public static void main(String[] args) {
+		
+//Declare and initialize
+    	//if
+    	  // true
+//    	  int age = 10;
+//    	  
+//    	  if (age==10) {
+//    		  
+//    		  System.out.println("Ram is below age 10");
+//			
+//		}
+//    	 //false
+//    	  if (age==9){
+//    		  
+//    		  System.out.println("Ram is not below age 10");
+//    		  
+//      }
+    	  
+    	 //Ladder
+//    	  int a = 10;
+//    	  int b = 12;
+//    	  int c = 15;
+//    	  
+//    	    		  
+//    	   if (a==20) {
+//    		   System.out.println(" A is 10");
+//    		  
+//    	   }
+//    	   else if (b==90) {
+//    		   System.out.println("B is 12");
+//    		   }
+//    	   
+//    	   else if (c==15) {
+//			System.out.println("C is 13");
+//		}
+//    	   else {
+//			System.out.println("Condition fail");
+//		}
+
+    	  
+    	  //Nested
+//    	   int a= 10;
+//    	   int b= 15;
+//    	   int c = 20;
+//    	   if (a==10) {
+//    		   
+//		   if (b==15) {
+//			   System.out.println("Condition is pass");
+//			   }
+//		   else {
+//			System.out.println("Cond 3 fail ");
+//		}
+//		   
+//		   if (c==20) {
+//			   System.out.println("Condition is pass");
+//		}
+//		   else {
+//				System.out.println("Condition 2is fail");
+//			}
+//		}
+//    	   else {
+//			System.out.println("Condition is 1 fail");
+//		}
+//      }
+    	  
+    	
+    	  
+    	  
+    	  
+    	  
+      }
+}
+    			   
+    			   
+    	  
+      
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+
